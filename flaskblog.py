cef6ded9f2c5479048b1733cf67ab48decb68c7a
Andy Glover <andrew.l.glover@gmail.com>
2024-05-16 09:03:36 -0700

    Initialize SQLAlchemy in Flask application

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = '46e8e5a05609c89707c1299f8aaace4f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
 posts = [
     {
